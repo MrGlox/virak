1913829998548f9d04844722b85d9d7b3708a193
Morgan <contact@morgan-leroux.com>
2020-10-11 20:40:55 +0200

    add workin loading

--- components/Loading.vue
+++ components/Loading.vue
@@ -1,105 +1,124 @@
 <template>
-  <div class="interaction">
-    <transition
-      appear
-      @enter="enter"
-      @before-enter="beforeEnter"
-      @after-enter="afterEnter"
-      @leave="leave"
-    >
-      <div class="content content--first">
-        <div class="content__move content__move--scroll-locked">
-          <div class="content__reverse animations">
-            <div class="animations__title-container">
-              <p ref="animationTitle" class="animations__title">I'm Virak</p>
-            </div>
-          </div>
+  <transition appear @enter="enter" @before-enter="beforeEnter" @leave="leave">
+    <div ref="container" :style="rotation" class="overlay overlay--container">
+      <div
+        ref="inner"
+        :style="translation"
+        class="overlay__move overlay__move--scroll-locked"
+      >
+        <div class="overlay__content">
+          <p>I'm Virak</p>
         </div>
       </div>
-    </transition>
-    <Pointer :multiply="multiply" />
-  </div>
+    </div>
+  </transition>
 </template>
 
 <script>
-import anime from 'animejs'
-import BezierEasing from 'bezier-easing'
-
-import Revealer from '~/assets/utils/revealer'
-import splitText from '~/assets/utils/split-text'
-import textAnimation from '~/assets/utils/text-animation'
+import { gsap } from 'gsap'
 
 export default {
   data() {
     return {
-      multiply: false,
+      angle: 8,
     }
   },
-  methods: {
-    beforeEnter(el) {
-      this.timeline = anime.timeline()
-      this.revealer = new Revealer(el, { angle: 12 })
-      this.customEasing = BezierEasing(0.83, -0.44, 0.2, 1)
+  computed: {
+    rotation() {
+      let style = `
+        width: ${
+          120 * Math.abs(Math.cos((this.angle * Math.PI) / 180)) +
+          120 * Math.abs(Math.sin((this.angle * Math.PI) / 180))
+        }%;
+      `
 
-      // Remove main title hide
-      this.$refs.animationTitle.style.opacity = 1
+      style += `
+        height: ${
+          110 * Math.abs(Math.sin((this.angle * Math.PI) / 180)) +
+          110 * Math.abs(Math.cos((this.angle * Math.PI) / 180))
+        }%;
+      `
+
+      style += `
+        transform: translateX(-50%) translateY(-50%) rotate3d(0,0,1,${this.angle}deg);
+      `
+
+      return style
     },
-    enter(el, done) {
-      const titleSplit = splitText(this.$refs.animationTitle)
+    translation() {
+      return `
+        transform: rotate3d(0,0,1,-${this.angle}deg);
+      `
+    },
+  },
+  methods: {
+    enter() {
+      // Pointer events related class
+      this.$refs.container.classList.add('overlay--hidden')
 
-      textAnimation(
-        this.timeline,
-        titleSplit.slice(0, titleSplit.length - 1),
-        '',
-        {
-          translateY: [100, 0],
-        }
-      )
-
-      textAnimation(this.timeline, titleSplit[titleSplit.length - 1], '+=200', {
-        translateY: [100, 0],
-        delay: anime.stagger(40),
-        begin: () => {
-          anime({
-            targets: [
-              ...titleSplit.slice(1, 7),
-              ...titleSplit.slice(8, 10),
-              ...titleSplit.slice(11, 18),
-              ...titleSplit.slice(19, 21),
-            ],
-            width: (el) => [el.offsetWidth, 0],
-            easing: () => (t) => this.customEasing(t),
-          })
-        },
+      // "Unreveal effect" (inner moves to one direction and reverse moves to the opposite one)
+      gsap.to(this.$refs.container, {
+        duration: 1.8,
+        ease: 'expo.inOut',
+        y: '-110%',
       })
 
-      this.timeline
-        .add(
-          {
-            targets: this.revealer.DOM.inner,
-            duration: 1200,
-            easing: 'easeInQuart',
-            translateY: [0, '-100%'],
-          },
-          '-=700'
-        )
-        .add(
-          {
-            targets: this.revealer.DOM.reverse,
-            duration: 1200,
-            easing: 'easeInQuart',
-            translateX: [0, '-16%'],
-            translateY: [0, '100%'],
-            complete: done,
-          },
-          '-=1200'
-        )
+      gsap.to(this.$refs.inner, {
+        duration: 1.8,
+        ease: 'expo.inOut',
+        y: '140%',
+      })
     },
-    afterEnter(el) {
-      el.style.display = 'none'
-      this.multiply = true
+    beforeEnter() {},
+    completeEnter() {
+      // if (this.$refs.inner) this.$refs.inner.style.display = 'none'
     },
     leave() {},
   },
 }
 </script>
+
+<style lang="scss" scoped>
+.overlay {
+  $el: &;
+
+  &--container {
+    position: fixed;
+    z-index: 99;
+    top: 50%;
+    left: 50%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+
+    width: 100vw;
+    min-height: 100vh;
+    background-color: $primary;
+  }
+
+  &--hidden {
+    overflow: hidden;
+  }
+
+  &__move {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100vw;
+    min-height: 100vh;
+  }
+
+  &__content {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    font-size: 40px;
+    color: $lightcolor;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
 <template>
-  <main>
-    <h1 class="title">Virak Mey Product Designer</h1>
-    <p class="subtitle">Lorem ipsum</p>
-  </main>
+  <div class="wrap home">
+    <div class="content content--second">
+      <div class="content__move">
+        <div class="content__reverse">
+          <div class="main">
+            <h1 class="title">Virak Mey Product Designer</h1>
+            <p class="subtitle">Lorem ipsum</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
